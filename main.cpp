949076171e044fcec8cce9972075ddf0404f670a
Nikita Miroshnichenko <nikita.miroshnichenko@yahoo.com>
2025-08-16 01:43:58 +0200

    added siple argument parsing to the compiler

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "MiniJavaLexerImpl.hpp"
+#include "fmt/base.h"
 #include "minijavaparser.gen.hpp"
+#include <fstream>
 
-int main()
+int main(int argc, char** argv)
 {
+    if(argc < 2)
+    {
+        fmt::println(stderr, "usage: ./minijavac <path/to/source/file>");
+        return 0;
+    }
+
+    std::ifstream fs{argv[1]};
+    if(!fs.is_open())
+    {
+        fmt::println(stderr, "Failed to open \"{}\" source file", argv[1]);
+        return 0;
+    }
+    
     MiniJavaLexerImpl lexer{};
+    lexer.switch_streams(&fs);
     yy::parser parser(lexer);
-    return parser();
+    return parser.parse();
 }
